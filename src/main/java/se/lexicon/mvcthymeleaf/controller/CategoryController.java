252454a43d2835f09f5982f68ea21442dccce41f
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-07-06 11:27:32 +0200

    implement delete operation with display alert message through RedirectAttributes

--- src/main/java/se/lexicon/mvcthymeleaf/controller/CategoryController.java
+++ src/main/java/se/lexicon/mvcthymeleaf/controller/CategoryController.java
@@ -3,6 +3,7 @@ package se.lexicon.mvcthymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import se.lexicon.mvcthymeleaf.model.dto.CategoryView;
 
 import java.time.LocalDate;
@@ -17,6 +18,7 @@ public class CategoryController {
     static List<CategoryView> categoryViews = new ArrayList<>();
 
     public CategoryController() {
+        System.out.println("default data loaded");
         categoryViews.add(new CategoryView(1, "book", LocalDate.now()));
         categoryViews.add(new CategoryView(2, "laptop", LocalDate.now()));
         categoryViews.add(new CategoryView(3, "mobile", LocalDate.now()));
@@ -46,6 +48,16 @@ public class CategoryController {
         model.addAttribute("categoryView", categoryView);
 
         return "category/category-view";
+    }
+
+
+    @GetMapping("/delete/{id}")
+    public String deleteById(@PathVariable("id") Integer id, RedirectAttributes redirectAttributes) {
+        System.out.println("ID:" + id);
+        categoryViews.removeIf(view -> view.getId() == id);
+        redirectAttributes.addFlashAttribute("message", "Category Id " + id + " was successfully deleted");
+        redirectAttributes.addFlashAttribute("alertClass", "alert alert-info");
+        return "redirect:/category/list";
     }
 
 }
